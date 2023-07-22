5723adcc12a32131ad33219a96f71793125514f8
Raphaël Malet <malet.raphael@gmail.com>
2023-07-22 16:02:18 +0200

    almost done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import React, { useState } from "react";
+const firstContacts = contacts.slice(0, 5);
 
 function App() {
+  const [firstFiveContacts, setFirstFiveContacts] = useState(firstContacts);
+  const randomNumberMinimumFive = () =>
+    Math.floor(Math.random() * (contacts.length + 1 - 5) + 5);
+  const selectRandomContact = () => {
+    let randomContact = contacts[randomNumberMinimumFive()];
+    let newListOfContacts = [...firstFiveContacts];
+    newListOfContacts.push(randomContact);
+    setFirstFiveContacts(newListOfContacts);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th colSpan="5">IronContacts</th>
+          </tr>
+          <tr>
+            <th colSpan="5">
+              <button onClick={selectRandomContact}>Add Random Contact</button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Picture</td>
+            <td>Name</td>
+            <td>Popularity</td>
+            <td>Won an Oscar</td>
+            <td>Won an Emmy</td>
+          </tr>
+          {firstFiveContacts.map((contact) => {
+            return (
+              <tr>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    alt={`photoshoot of ${contact.name}`}
+                    style={{ width: "100px", height: "150px" }}
+                  ></img>
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                <td>{contact.wonOscar ? "🏆" : ""}</td>
+                <td>{contact.wonEmmy ? "🏆" : ""}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
